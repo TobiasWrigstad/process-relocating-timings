e18cb515704f634f48d0c2021f5dd6ccfd42c333
Tobias Wrigstad <tobias.wrigstad@it.uu.se>
2022-02-13 10:25:50 +0100

    Fast

--- histogram.go
+++ histogram.go
@@ -8,7 +8,14 @@ import (
 	"os"
 	"strconv"
 	"io"
+	"strings"
 )
+
+type worker_result struct {
+	max int64
+	min int64
+	results map[int64]int64
+}
 
 func worker(depth int, lines chan string, bucket_size int64, results chan map[int64]int64) {
 	var result_from_children chan map[int64]int64
@@ -39,18 +46,29 @@ func aggregate_results_from_children(buckets map[int64]int64, results_from_child
 
 func do_work(lines chan string, bucket_width int64) map[int64]int64 {
 	buckets := make(map[int64]int64)
+	max := int64(0)
+	min := int64(0)
+	
 	for {
-		line, ok := <-lines
-		if ok {
-			i, err := strconv.ParseInt(line, 10, 64)
-			if err == nil {
-				if i < 0 { // Channel is closed by sending -1 to all workers
-					break
+		data, ok := <-lines
+		if data == "STOP" {
+			break
+		}
+		scanner := bufio.NewScanner(strings.NewReader(data))
+		for scanner.Scan() {
+			line := scanner.Text()
+			
+			if ok {
+				i, err := strconv.ParseInt(line, 10, 64)
+				if err == nil {
+					bucket := i / bucket_width
+					buckets[bucket] += 1
+
+					if i > max { max = i }
+					if i < min { min = i }
+				} else {
+					fmt.Printf("Parse error for '%s' -- ignored\n", line)
 				}
-				bucket := i / bucket_width
-				buckets[bucket] += 1	
-			} else {
-				fmt.Printf("Parse error for '%s' -- ignored\n", line)
 			}
 		}
 	}
@@ -59,7 +77,7 @@ func do_work(lines chan string, bucket_width int64) map[int64]int64 {
 }
 
 func main() {
-	stop_signal := "-1"
+	stop_signal := "STOP"
 	bucket_size := flag.Int("w", 50, "The width of a bucket")
 	depth := flag.Int("d", 4, "Depth of worker tree (number of workers = 2^{d+1}-1)")
 	file_name := flag.String("f", "test.txt", "Name of file with data")
@@ -84,9 +102,10 @@ func main() {
 	scanner.Buffer(buf, bufio.MaxScanTokenSize)
 	//scanner.Split(bufio.ScanLines)
 
-	for scanner.Scan() {
-		backlog<-scanner.Text()
-	}
+	read_chunk(file, backlog)
+	// for scanner.Scan() {
+	// 	backlog<-scanner.Text()
+	// }
 
 	for i := 0; i < workers; i += 1 {
 		backlog<-stop_signal
@@ -94,7 +113,13 @@ func main() {
 	
 	file.Close()
 
-	fmt.Println("result:", <-final)
+	result := make(map[int64]int64)
+	b, _ := <-final
+	for k,v := range b {
+		result[k * int64(*bucket_size)] = v
+	}
+
+	fmt.Println("result:", result)
 
 	fmt.Println("---------------------------------")
 	fmt.Println("file:         ", *file_name)
@@ -102,17 +127,16 @@ func main() {
 	fmt.Println("workers:      ", workers)
 }
 
-func read_chunk(f os.File, sink chan string) {
-	defer f.Close()
-	buf := make([]byte, 1024*1024)
+func read_chunk(f *os.File, sink chan string) {
+	buf := make([]byte, 1024 * 1024)
 	for {
 		n, err := f.Read(buf)
 		if err == io.EOF {
 			break
 		}
-		if err != nil {
-			sink<-string(buf[:n])
-			continue
-		}
+		s := string(buf[:n])
+		overshoot := strings.LastIndex(s, "\n")
+		f.Seek(int64(overshoot - n + 1), 1)
+		sink<-string(buf[:overshoot])
 	}
 }
