849f4a380ec27d35e7bc519f11eca90ae1f6165d
Tobias Wrigstad <tobias.wrigstad@it.uu.se>
2022-02-14 08:10:12 +0100

    V1.0
    Added:
    
    - quartile printing
    - printing n buckets
    
    Bug fixes:
    
    - Fixed a non-deterministic bug due to accidental (sneaky) value
      semantics that screwed up the max value

--- histogram.go
+++ histogram.go
@@ -14,40 +14,47 @@ import (
 type worker_result struct {
 	max int64
 	min int64
-	results map[int64]int64
+	data_points int64
+	buckets map[int64]int64
 }
 
-func worker(depth int, lines chan string, bucket_size int64, results chan map[int64]int64) {
-	var result_from_children chan map[int64]int64
+func worker(depth int, lines chan string, bucket_size int64, results chan worker_result) {
+	var result_from_children chan worker_result
 	
 	if depth > 0 {
-		result_from_children = make(chan map[int64]int64, 2)
+		result_from_children = make(chan worker_result, 2)
 		go worker(depth - 1, lines, bucket_size, result_from_children)
 		go worker(depth - 1, lines, bucket_size, result_from_children)
 	}
 
-	buckets := do_work(lines, bucket_size)
+	result := do_work(lines, bucket_size)
 	
 	if depth > 0 {
-		aggregate_results_from_children(buckets, result_from_children)
+		result = aggregate_results_from_children(result, result_from_children)
 	}
 
-	results<-buckets
+	results<-result
 }
 
-func aggregate_results_from_children(buckets map[int64]int64, results_from_children chan map[int64]int64) {
+func aggregate_results_from_children(result worker_result, results_from_children chan worker_result) worker_result {
 	for i := 0; i < 2; i += 1 {
 		b, _ := <-results_from_children
-		for k,v := range b {
-			buckets[k] += v
+		for k,v := range b.buckets {
+			result.buckets[k] += v
 		}
+		if b.max > result.max { result.max = b.max }
+		if b.min < result.min { result.min = b.min }
+		result.data_points += b.data_points
 	}
+
+	return result
 }
 
-func do_work(lines chan string, bucket_width int64) map[int64]int64 {
+func do_work(lines chan string, bucket_width int64) worker_result {
 	buckets := make(map[int64]int64)
 	max := int64(0)
-	min := int64(0)
+	min := int64(9223372036854775807) // TODO: INTMAX
+	data_points := int64(0)
 	
 	for {
 		data, ok := <-lines
@@ -63,6 +70,7 @@ func do_work(lines chan string, bucket_width int64) map[int64]int64 {
 				if err == nil {
 					bucket := i / bucket_width
 					buckets[bucket] += 1
+					data_points += 1
 
 					if i > max { max = i }
 					if i < min { min = i }
@@ -73,53 +81,64 @@ func do_work(lines chan string, bucket_width int64) map[int64]int64 {
 		}
 	}
 
-	return buckets
+	return worker_result{max: max, min: min, data_points: data_points, buckets: buckets}
 }
 
 func main() {
 	stop_signal := "STOP"
 	bucket_size := flag.Int("w", 50, "The width of a bucket")
 	depth := flag.Int("d", 4, "Depth of worker tree (number of workers = 2^{d+1}-1)")
-	file_name := flag.String("f", "test.txt", "Name of file with data")
+	print := flag.Int("p", 10, "Number of low buckets to print")
+	file_name := flag.String("f", "<no filename given>", "Name of file with data")
 	flag.Parse()
 
 	workers := (2 << *depth) - 1 // 2^{depth-1} - 1
 	
-	final := make(chan map[int64]int64)
-	backlog := make(chan string, 128)
+	final := make(chan worker_result)
+	backlog := make(chan string, workers * 2)
 	
 	file, err := os.Open(*file_name)
 
 	if err != nil {
-		log.Fatalf("failed to open")
+		log.Fatalf("failed to open %s\n", *file_name)
 	}
 
 	// Create worker tree in parallel
 	go worker(*depth, backlog, int64(*bucket_size), final)
-	
-	scanner := bufio.NewScanner(file)
-	buf := make([]byte, 64*1024)
-	scanner.Buffer(buf, bufio.MaxScanTokenSize)
-	//scanner.Split(bufio.ScanLines)
-
+	// Fill work log for workers
 	read_chunk(file, backlog)
-	// for scanner.Scan() {
-	// 	backlog<-scanner.Text()
-	// }
-
+	// Tell all workers to stop
 	for i := 0; i < workers; i += 1 {
 		backlog<-stop_signal
 	}
 	
-	file.Close()
+	defer file.Close()
 
-	result := make(map[int64]int64)
+	quartiles := make(map[int64]int64)
 	b, _ := <-final
-	for k,v := range b {
-		result[k * int64(*bucket_size)] = v
+	quartile_width := b.max / 4 / int64(*bucket_size) + 1
+	sum := int64(0) 
+	
+	for k,v := range b.buckets {
+		quartiles[k / quartile_width] += v
+		sum += v
 	}
 
-	fmt.Println("result:", result)
+	fmt.Println("max:    ", b.max)
+	fmt.Println("min:    ", b.min)
+	fmt.Println("data points:    ", b.data_points)
+	fmt.Println("quartile width: ", quartile_width * int64(*bucket_size))
+	for qi := 0; qi < 4; qi += 1 {
+		fmt.Printf("q%v:      %v%%\n", qi + 1, float64(quartiles[int64(qi)]) / float64(sum) * 100)
+	}
+
+	fmt.Printf("---------------------------------\nBuckets 1-%v\n", *print)
+	low := 0
+	for bi := 0; bi < *print; bi += 1 {
+		high := low + *bucket_size
+		fmt.Printf("[%v,%v):\t%v\n", low, high, b.buckets[int64(bi)])
+		low = high
+	}
 
 	fmt.Println("---------------------------------")
 	fmt.Println("file:         ", *file_name)
